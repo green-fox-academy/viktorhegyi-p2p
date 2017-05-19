69ebbfb32dcf85c885b96e956cecedc3fd5ca8dd
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-19 12:35:39 +0200

    commit

--- src/main/java/com/greenfox/chat/controller/RestMainController.java
+++ src/main/java/com/greenfox/chat/controller/RestMainController.java
@@ -2,17 +2,12 @@ package com.greenfox.chat.controller;
 
 import com.greenfox.chat.model.Json;
 import com.greenfox.chat.model.Status;
-import com.greenfox.chat.service.JsonService;
 import com.greenfox.chat.repository.MessageRepo;
-import java.sql.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Viktor on 2017-05-17.
