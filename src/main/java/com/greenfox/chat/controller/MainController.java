616d6553fd3c0204a5f227dd7fc7856416e774bb
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-25 16:51:33 +0200

    Add delete function, add placeholder

--- src/main/java/com/greenfox/chat/controller/MainController.java
+++ src/main/java/com/greenfox/chat/controller/MainController.java
@@ -85,4 +85,10 @@ public class MainController {
     return "redirect:/";
   }
 
+  @RequestMapping(value = "/delete")
+  public String delete() {
+    messageRepo.deleteAll();
+    return "redirect:/";
+  }
+
 }
